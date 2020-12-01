d859123f30dfea6a545a35a9982f11c08303c7b4
Tony Gallotta <anthony.gallotta@gmail.com>
2020-12-01 09:00:02 -0600

    Cleanup

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let reader = io::BufReader::new(file);
     let mut expenses = Vec::new();
     for (_, line) in reader.lines().enumerate() {
-        let expense:u32 = line.unwrap().parse().unwrap(); // Ignore errors.
+        let expense: u32 = line.unwrap().parse().unwrap(); // Ignore errors.
         expenses.push(expense);
     }
     let summing_pair = sum_to_2020_pair(&expenses);
@@ -25,33 +25,34 @@ fn main() {
 }
 
 fn sum_to_2020_pair(expenses: &Vec<u32>) -> (u32, u32) {
-    for e1 in expenses.iter() {
-        for e2 in expenses.iter() {
+    for (i, e1) in expenses.iter().enumerate() {
+        for e2 in expenses.iter().skip(i) {
             if (e1 + e2) == 2020 {
                 println!("{} + {} = 2020", e1, e2);
-                return (*e1, *e2)
+                return (*e1, *e2);
             }
         }
     }
-    return (0, 0)
+    return (0, 0);
 }
 
 
 fn sum_to_2020_triplet(expenses: &Vec<u32>) -> (u32, u32, u32) {
-    for e1 in expenses.iter() {
-        for e2 in expenses.iter() {
-            for e3 in expenses.iter() {
+    for (i, e1) in expenses.iter().enumerate() {
+        for (j, e2) in expenses.iter().skip(i).enumerate() {
+            for e3 in expenses.iter().skip(j) {
                 if (e1 + e2 + e3) == 2020 {
                     println!("{} + {} + {} = 2020", e1, e2, e3);
-                    return (*e1, *e2, *e3)
+                    return (*e1, *e2, *e3);
                 }
             }
         }
     }
-    return (0, 0, 0)
+    return (0, 0, 0);
 }
 
 #[test]
 fn test() {
-    assert_eq!(2, 4/2);
+    let sample_data = vec![1721, 979, 366, 299, 675, 1456];
+    assert_eq!((1721, 299), sum_to_2020_pair(&sample_data));
 }
