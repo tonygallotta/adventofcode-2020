62eb5a6263ef3ff3824e681b11e873e5afaf3b0b
Tony Gallotta <anthony.gallotta@gmail.com>
2020-12-04 09:00:02 -0600

    Day 4 cleaned up

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,110 +2,129 @@ use std::env;
 use std::fs;
 use std::io;
 use std::io::BufRead;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::iter::FromIterator;
 use regex::Regex;
 
-fn main() {
-    let filename = env::args().nth(1).unwrap_or(String::from("input.txt"));
-    let parsed_lines = read_file_to_vec(filename);
-    let mut required_fields: HashSet<String> = HashSet::new();
-    required_fields.insert(String::from("byr")); // (Birth Year)
-    required_fields.insert(String::from("iyr")); // (Issue Year)
-    required_fields.insert(String::from("eyr")); // (Expiration Year)
-    required_fields.insert(String::from("hgt")); // (Height)
-    required_fields.insert(String::from("hcl")); // (Hair Color)
-    required_fields.insert(String::from("ecl")); // (Eye Color)
-    required_fields.insert(String::from("pid")); // (Passport ID)
-    //required_fields.insert("cid"); // (Country ID)
+struct Passport {
+    field_map: HashMap<String, String>
+}
 
-    let hcl_regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
-    let pid_regex = Regex::new(r"^[0-9]{9}$").unwrap();
-    let mut valid_ecls = HashSet::new();
-    valid_ecls.insert("amb");
-    valid_ecls.insert("blu");
-    valid_ecls.insert("brn");
-    valid_ecls.insert("gry");
-    valid_ecls.insert("grn");
-    valid_ecls.insert("hzl");
-    valid_ecls.insert("oth");
-
-    let mut part_1_answer = 0;
-    for (_, passport_block) in parsed_lines.iter().enumerate() {
-        // println!("Passport {}: \n{}", i, passport_block.join("\n"));
-        let mut passport_fields: HashSet<String> = HashSet::new();
-        let mut valid: bool = true;
-        for (_, line) in passport_block.iter().enumerate() {
-            // Vec::from_iter(line.split(" ").map(String::from));
-            let key_value_pairs = Vec::from_iter(line.split(" ").map(String::from));
+impl Passport {
+    fn from(lines: &Vec<String>) -> Passport {
+        let mut field_map = HashMap::new();
+        for line in lines.iter() {
+            let key_value_pairs = Vec::from_iter(line.split(" "));
             for (_, key_value_pair) in key_value_pairs.iter().enumerate() {
-                let split_kvp: Vec<String> = Vec::from_iter(key_value_pair.split(":").map(String::from));
-                let key = &split_kvp[0];
-                let value = &split_kvp[1];
-                passport_fields.insert(key.clone());
-                let is_value_valid = match key.as_str() {
-                    "byr" => {
-                        let numeric_value = value.parse::<i32>().unwrap();
-                        numeric_value >= 1920 && numeric_value <= 2002
-                    }
-                    "iyr" => {
-                        let numeric_value = value.parse::<i32>().unwrap();
-                        numeric_value >= 2010 && numeric_value <= 2020
-                    }
-                    "eyr" => {
-                        let numeric_value = value.parse::<i32>().unwrap();
-                        numeric_value >= 2020 && numeric_value <= 2030
-                    }
-                    "hgt" => {
-                        let units = value.chars().nth(value.len() - 2).unwrap_or('_');
-                        let numeric_value = value.replace("in", "").replace("cm", "").parse::<u32>().unwrap();
-                        println!("Checking height block {}, units={}, value={}", key_value_pair, units, numeric_value);
-                        let mut result = false;
-                        if units == 'i' {
-                            result = numeric_value >= 59 && numeric_value <= 76;
-                        } else if units == 'c' {
-                            result = numeric_value >= 150 && numeric_value <= 193;
-                        }
-                        result
-                    }
-                    "hcl" => hcl_regex.is_match(value),
-                    "ecl" => valid_ecls.contains(value.as_str()),
-                    "pid" => pid_regex.is_match(value),
-                    _ => true
-                };
-                valid = valid && is_value_valid
+                let split_kvp: Vec<&str> = Vec::from_iter(key_value_pair.split(":"));
+                let key = split_kvp[0];
+                let value = split_kvp[1];
+                field_map.insert(String::from(key), String::from(value));
             }
         }
-        if valid && required_fields.is_subset(&passport_fields) {
+        Passport { field_map }
+    }
+
+    fn has_all_required_fields(&self) -> bool {
+        // Not sure how to make this more of a constant
+        let required_fields: HashSet<&str> = vec!("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid").into_iter().collect();
+        let field_map = &self.field_map;
+        let present_keys: HashSet<&str> = field_map.keys().map(String::as_str).collect();
+        required_fields.is_subset(&present_keys)
+    }
+
+    fn present_fields_valid(&self) -> bool {
+        let hcl_regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+        let pid_regex = Regex::new(r"^[0-9]{9}$").unwrap();
+        let valid_ecls : HashSet<&str> = vec!("amb", "blu", "brn", "gry", "grn", "hzl", "oth").into_iter().collect();
+        let field_map = &self.field_map;
+        let mut is_valid = true;
+
+        for (key, value) in field_map {
+            is_valid = is_valid && match key.as_str() {
+                "byr" => {
+                    let numeric_value = value.parse::<i32>().unwrap();
+                    numeric_value >= 1920 && numeric_value <= 2002
+                }
+                "iyr" => {
+                    let numeric_value = value.parse::<i32>().unwrap();
+                    numeric_value >= 2010 && numeric_value <= 2020
+                }
+                "eyr" => {
+                    let numeric_value = value.parse::<i32>().unwrap();
+                    numeric_value >= 2020 && numeric_value <= 2030
+                }
+                "hgt" => {
+                    let units = value.chars().nth(value.len() - 2).unwrap_or('_');
+                    let numeric_value = value.replace("in", "").replace("cm", "").parse::<u32>().unwrap();
+                    let mut result = false;
+                    if units == 'i' {
+                        result = numeric_value >= 59 && numeric_value <= 76;
+                    } else if units == 'c' {
+                        result = numeric_value >= 150 && numeric_value <= 193;
+                    }
+                    result
+                },
+                "hcl" => hcl_regex.is_match(value),
+                "ecl" => valid_ecls.contains(value.as_str()),
+                "pid" => pid_regex.is_match(value),
+                _ => true
+            }
+        }
+        is_valid
+    }
+}
+
+fn main() {
+    let filename = env::args().nth(1).unwrap_or(String::from("input.txt"));
+    let passports = read_file_to_vec(filename);
+    let mut part_1_answer = 0;
+    let mut part_2_answer = 0;
+
+    for passport in passports {
+        if passport.has_all_required_fields() {
             part_1_answer += 1;
+            if passport.present_fields_valid() {
+                part_2_answer += 1
+            }
         }
     }
 
     println!("PART 1: {}", part_1_answer);
+    println!("PART 2: {}", part_2_answer);
 }
 
-fn read_file_to_vec(filename: String) -> Vec<Vec<String>> {
+fn read_file_to_vec(filename: String) -> Vec<Passport> {
     println!("Reading file {}", filename);
     let file = fs::File::open(filename).unwrap();
     let reader = io::BufReader::new(file);
-    let mut parsed_lines: Vec<Vec<String>> = Vec::new();
+    let mut parsed_lines: Vec<Passport> = Vec::new();
     let mut passport_block: Vec<String> = Vec::new();
     for (_, line) in reader.lines().enumerate() {
         let unwrapped = line.unwrap();
         if unwrapped.is_empty() {
-            parsed_lines.push(passport_block.clone());
+            parsed_lines.push(Passport::from(&passport_block));
             passport_block.clear();
         } else {
             passport_block.push(unwrapped);
         }
     }
-    parsed_lines.push(passport_block);
+    parsed_lines.push(Passport::from(&passport_block));
     return parsed_lines;
 }
 
 #[test]
-fn test() {
-    let sample_lines = read_file_to_vec(String::from("sample_input.txt"));
-    assert_eq!(7, part1(&sample_lines, 66, 3, 1));
+fn test_part1() {
+    let passports = read_file_to_vec(String::from("sample_input_1.txt"));
+    assert_eq!(2, passports.iter().filter(|&p| p.has_all_required_fields()).count());
+}
+
+#[test]
+fn test_part2() {
+    let invalid_passports = read_file_to_vec(String::from("sample_input_2.txt"));
+    assert_eq!(0, invalid_passports.iter().filter(|&p| p.has_all_required_fields() && p.present_fields_valid()).count());
+
+    let valid_passports = read_file_to_vec(String::from("sample_input_3.txt"));
+    assert_eq!(4, valid_passports.iter().filter(|&p| p.has_all_required_fields() && p.present_fields_valid()).count());
 }
 
