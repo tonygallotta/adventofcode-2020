00f9133efcc6e733c72524561a75991db4a27db7
Tony Gallotta <anthony.gallotta@gmail.com>
2020-12-15 08:40:13 -0600

    Day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,18 +1,11 @@
 use std::collections::HashMap;
-use std::env;
-use std::fs;
-use std::io;
-use std::io::BufRead;
 use std::time::Instant;
 
 // PART 1: 662
 // PART 2: 37312
-// Execution completed in 30573ms
+// Execution completed in 17580ms
 fn main() {
     let timer = Instant::now();
-    let filename = env::args()
-        .nth(1)
-        .unwrap_or_else(|| String::from("input.txt"));
     let (part_1_answer, part_2_answer) = answers(&vec![16, 11, 15, 0, 1, 7]);
 
     println!("PART 1: {}", part_1_answer);
@@ -30,11 +23,10 @@ fn answers(lines: &Vec<u64>) -> (u64, u64) {
 fn nth_spoken_number(starting_numbers: &Vec<u64>, n: u64) -> u64 {
     let mut history: HashMap<u64, u64> = HashMap::new();
     let mut last_number_spoken = 0;
-    let mut last_time_spoken: Option<u64>;
+    let mut last_time_spoken: Option<u64> = Option::None;
     for turn in 0..n {
-        last_time_spoken = history.get(&last_number_spoken).cloned();
         if turn > 0 {
-            history.insert(last_number_spoken.clone(), turn);
+            last_time_spoken = history.insert(last_number_spoken, turn);
         }
         if turn < starting_numbers.len() as u64 {
             last_number_spoken = starting_numbers.get(turn as usize).unwrap().clone();
